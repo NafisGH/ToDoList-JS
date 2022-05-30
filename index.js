65dc9cbcbbfb38c9e91496893bd40ef2bed9bb3d
Nafis Garifullin <nafis-86@inbox.ru>
2022-05-30 22:25:38 +0500

    feat: wrote a function to add elements to a list

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
-// font-family: 'Lobster Two', cursive;
-// font-family: 'Lora', serif;
+const inputAddTask = document.querySelector('.to-do-list__input_type_add-task');
+const buttonSubmitAddTask = document.querySelector('.to-do-list__button_type_add-task');
+const TaskList = document.querySelector('.to-do-list__task_list');
+
+const taskItemTemplate = document.querySelector('#task-item-template').content;
+
+let valueInputAddTask = '';
+let tasks = [
+    { text: 'Полить цветы'},
+    { text: 'Сходить в магазин'},
+]
+
+tasks.forEach((item, index) => {
+    const newTaskItem = taskItemTemplate.querySelector('.to-do-list__task_item').cloneNode(true);
+    const taskItemInput = newTaskItem.querySelector('.to-do-list__input_task-item');
+    const taskItemNumber = newTaskItem.querySelector('.to-do-list__task-item-number'); 
+
+    taskItemInput.value = item.text;
+    taskItemNumber.textContent = 2;
+   
+    TaskList.append(newTaskItem);
+})
+
+inputAddTask.addEventListener('input', (event) => {
+    valueInputAddTask = event.target.value;
+})
+
+buttonSubmitAddTask.addEventListener('click', (event) => {
+event.preventDefault();
+console.log('Click');
+
+if(!valueInputAddTask) {
+    console.log('Поле пустое')
+} else {
+    const newTaskItem = taskItemTemplate.querySelector('.to-do-list__task_item').cloneNode(true);
+    const taskItemInput = newTaskItem.querySelector('.to-do-list__input_task-item');
+    const taskItemNumber = newTaskItem.querySelector('.to-do-list__task-item-number');
+
+    taskItemInput.value = valueInputAddTask;
+    taskItemNumber.textContent = 2;
+   
+    TaskList.append(newTaskItem);
+
+    inputAddTask.value = '';
+    valueInputAddTask = '';
+
+}
+})
